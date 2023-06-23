353b96cdc302436178f5d7c761034c6ff6ce8554
OlgaLegkikh <23OShe@gmail.com>
2023-06-23 22:31:26 +0300

    Start working on Colloseum

---
+++ Colloseum_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Colloseum_Game
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
